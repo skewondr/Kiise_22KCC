4770fd79cebe7d48be112abdfd14766764a008be
skewondr <skewondr@naver.com>
2022-07-16 16:11:27 +0900

    edit shell files

--- examples/run_model.sh
+++ examples/run_model.sh
@@ -2,25 +2,24 @@
 
 #!/usr/bin/env bash
 
-DATASET=assist2015
-
-k_emb=(5 25 50)
-for k in ${k_emb[@]}; do
-    args=(
-        --dataset_name $DATASET
-        --q2a ${k}
-        # --batch_size
-        # --num_epochs
-    )
-    echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_saint_train.py ${args[@]}`
-done
-
-for k in ${k_emb[@]}; do
-    args=(
-        --dataset_name $DATASET
-        --r2a ${k}
-        # --batch_size
-        # --num_epochs
-    )
-    echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+dataset=(assist2009)
+model=(dkt)
+seed=(1 2 3)
+for d in ${dataset[@]}; do
+    for m in ${model[@]}; do
+        for s in ${seed[@]}; do
+            args=(
+                --dataset_name ${d}
+                --model_name ${m}
+                --seed ${s}
+                --seq_len 200
+                --num_epochs 200
+                --es_patience 10
+                --fold 5
+                # --gpu --> you can change
+            )
+            # echo `PYTHONPATH=/workspace/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+            echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+        done
+    done
 done
--- examples/run_model2.sh
+++ examples/run_model2.sh
@@ -2,25 +2,26 @@
 
 #!/usr/bin/env bash
 
-DATASET=assist2015
+# PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python data_preprocess.py --dataset_name ednet 
 
-model=(dkt sakt gkt saint)
-for m in ${model[@]}; do
-    args=(
-        --dataset_name $DATASET
-        # --q2a ${k}
-        # --batch_size
-        # --num_epochs
-    )
-    echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_${m}_train.py ${args[@]}`
-done
-
-for k in ${k_emb[@]}; do
-    args=(
-        --dataset_name $DATASET
-        # --r2a ${k}
-        # --batch_size
-        # --num_epochs
-    )
-    echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+dataset=(assist2009)
+model=(dkt)
+seed=(1 2 3)
+for d in ${dataset[@]}; do
+    for m in ${model[@]}; do
+        for s in ${seed[@]}; do
+            args=(
+                --dataset_name ${d}
+                --model_name ${m}
+                --seed ${s}
+                --seq_len 200
+                --num_epochs 200
+                --es_patience 10
+                --fold 5
+                # --gpu --> you can change
+            )
+            # echo `PYTHONPATH=/workspace/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+            echo `PYTHONPATH=/home/tako/yoonjin/pykt-toolkit python wandb_saint_train.py ${args[@]}`
+        done
+    done
 done
