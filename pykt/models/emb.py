58fcbb96726dfe20f4124324a614e2cdccb241d4
skewondr <skewondr@naver.com>
2022-08-11 22:40:27 +0900

    add emb.py model

---
+++ pykt/models/emb.py
@@ -0,0 +1,25 @@
+import os
+
+import numpy as np
+import torch
+
+from torch.nn import Module, Embedding, LSTM, Linear, Dropout
+
+class EMB(Module):
+    def __init__(self, num_c, emb_size, dropout=0.1, emb_type="qid"):
+        super().__init__()
+        self.model_name = "emb"
+        self.num_c = num_c
+        self.emb_size = emb_size
+        self.input_emb = Embedding(self.num_c, self.emb_size)
+        self.dropout_layer = Dropout(dropout)
+        self.out_layer = Linear(self.emb_size, 1)
+        self.emb_type = emb_type
+        
+
+    def forward(self, q):
+        xemb = self.input_emb(q)
+        h = self.dropout_layer(xemb)
+        y = self.out_layer(h)
+
+        return y.squeeze()
